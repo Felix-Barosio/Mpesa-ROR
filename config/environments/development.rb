92c3bc6b4ee159f1b7c37b732a31f23162933155
Felix <barosiofelix@gmail.com>
2022-12-20 17:01:55 +0300

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,11 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.hosts.clear
+
+  # allow ngrok to work
+  # config.hosts << /[a-z0-9]+\.ngrok\.io/
+
   # Do not eager load code on boot.
   config.eager_load = false
 

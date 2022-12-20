a733ab4db38589d05c6b625b9c010dccb98a5ae9
Felix <barosiofelix@gmail.com>
2022-12-20 16:59:24 +0300

    add: get access token

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -18,4 +18,21 @@ class MpesasController < ApplicationController
         res
     end
 
+    def get_access_token
+
+        res = generate_access_token_request()
+        if res.code != 200
+            r = generate_access_token_request()
+            if res.code != 200
+                raise MpesaError('Unable to generate access token')
+            end
+        end
+
+        body = JSON.parse(res, { symbolize_names: true })
+        token = body[:access_token]
+        AccessToken.destroy_all()
+        AccessToken.create!(token: token)
+        token
+    end
+
 end

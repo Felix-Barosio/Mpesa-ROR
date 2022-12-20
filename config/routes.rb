37442a816f8b7f4b147ff64ce444fe986a3c6a41
Felix <barosiofelix@gmail.com>
2022-12-20 17:01:24 +0300

    add: routes stkpush

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :mpesas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  resources :mpesas
+  post "/stkpush", to: "mpesas#stkpush"
+
 end

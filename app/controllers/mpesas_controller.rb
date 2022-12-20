fa9362d123638b640f69c832642d2e4444668d86
Felix <barosiofelix@gmail.com>
2022-12-20 17:20:50 +0300

    add: stkpush

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -2,6 +2,58 @@ class MpesasController < ApplicationController
 
     require 'rest-client'
 
+    # stkpush
+    # make payment
+
+    def stkpush
+        phoneNumber = params[:phoneNumber]
+        amount = params[:amount]
+
+        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+        timestamp = "#{Time.now.strftime "%Y%m%d%H%M%S"}"
+        business_short_code = ENV["MPESA_SHORTCODE"]
+        password = Base64.strict_encode64("#{business_short_code}#{ENV["MPESA_PASSKEY"]}#{timestamp}")
+        payload = {
+            'BusinessShortCode': business_short_code,
+            'Password': password,
+            'Timestamp': timestamp,
+            'TransactionType': "CustomerPayBillOnline",
+            'Amount': amount,
+            'PartyA': phoneNumber,
+            'PartyB': business_short_code,
+            'PhoneNumber': phoneNumber,
+            'CallBackURL': "#{ENV["CALLBACK_URL"]}/callback_url",
+            'AccountReference': 'Mpesa_ROR',
+            'TransactionDesc': "Payment for Mpesa_ROR"
+        }.to_json
+
+        headers = {
+            Content_type: 'application/json',
+            Authorization: "Bearer #{get_access_token}"
+        }
+
+        response = RestClient::Request.new({
+            method: :post,
+            url: url,
+            payload: payload,
+            headers: headers
+            }).execute do |response, request|
+                case response.code
+                when 500
+                    [ :error, JSON.parse(response.to_str) ]
+                when 400
+                    [ :error, JSON.parse(response.to_str) ]
+                when 200
+                    [ :success, JSON.parse(response.to_str) ]
+                else
+                    fail "Invalid response #{response.to_str} received."
+                end
+            end
+        render json: response
+
+
+    end
+
     private
 
     def generate_access_token_request

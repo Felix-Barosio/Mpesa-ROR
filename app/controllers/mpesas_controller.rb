6841f5801f42c673ec0683ba924a8dc1d5a75aa9
Felix <barosiofelix@gmail.com>
2022-12-21 16:13:57 +0300

    add: query complete

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -54,6 +54,51 @@ class MpesasController < ApplicationController
 
     end
 
+
+    # stkquery
+    # confirm if payment is gone through
+
+    def stkquery
+        url = "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query"
+
+        timestamp = "#{Time.now.strftime "%Y%m%d%H%M%S"}"
+        business_short_code = ENV["MPESA_SHORTCODE"]
+        password = Base64.strict_encode64("#{business_short_code}#{ENV["MPESA_PASSKEY"]}#{timestamp}")
+        payload = {
+            'BusinessShortCode': business_short_code,
+            'Password': password,
+            'Timestamp': timestamp,
+             # Check if payment has been paid
+            'CheckoutRequestID': params[:checkoutRequestID]
+        }.to_json
+
+        headers = {
+            Content_type: 'application/json',
+            Authorization: "Bearer #{ get_access_token }"
+        }
+
+        response = RestClient::Request.new({
+            method: :post,
+            url: url,
+            payload: payload,
+            headers: headers
+            }).execute do |response, request|
+                case response.code
+                when 500
+                    [ :error, JSON.parse(response.to_str) ]
+                when 400
+                    [ :error, JSON.parse(response.to_str) ]
+                when 200
+                    [ :success, JSON.parse(response.to_str) ]
+                else
+                    fail "Invalid response #{response.to_str} received."
+                end
+            end
+        render json: response
+
+        
+    end
+
     private
 
     def generate_access_token_request
